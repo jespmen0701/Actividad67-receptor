2c88d871ec03b370b58dc2b7293e5ff7a5b587cc
jespmen0701 <132547356+jespmen0701@users.noreply.github.com>
2023-05-19 12:38:44 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "POWER") {
+        entendido = !(entendido)
+        led.setBrightness(125)
+        led.enable(entendido)
+    }
+})
+let entendido = false
+radio.setGroup(1)
+entendido = false
+led.enable(entendido)
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    `)
